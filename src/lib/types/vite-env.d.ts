d47899b96311faab12c90052f7515efdee358705
Marcus Gajozo <marcusgajozo@gmail.com>
2026-01-21 15:20:47 -0400

    fix: adjust type

--- src/lib/types/vite-env.d.ts
+++ src/lib/types/vite-env.d.ts
@@ -1,10 +1,9 @@
+/// <reference types="vite/client" />
+
 interface Window {
   _env_: {
     [key: string]: string | undefined;
   };
 }
-
-
-/// <reference types="vite/client" />
 
 declare const __LOCAL_IP__: string;
